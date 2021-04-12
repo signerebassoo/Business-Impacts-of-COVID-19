28e5ea32e4e52ad0ba3d57737670805876331b42
signerebassoo <signe.rebassoo@hotmail.com>
2021-04-12 04:26:40 +0100

    Government Scheme queries and graph for workforce

--- assets/js/gov-scheme-wf-chart.js
+++ assets/js/gov-scheme-wf-chart.js
@@ -1,57 +1,57 @@
 // Dimensions and margins
-var margin_gov_g = {top: 10, right: 0, bottom: 260, left: 50},
-    width_gov_g = document.getElementById("gov-schemes").parentElement.offsetWidth - margin_gov_g.left - margin_gov_g.right,
-    height_gov_g = 700 - margin_gov_g.top - margin_gov_g.bottom;
+var margin_gov_wf_g = {top: 10, right: 75, bottom: 260, left: 50},
+    width_gov_wf_g = document.getElementById("gov-schemes-wf").parentElement.offsetWidth - margin_gov_wf_g.left - margin_gov_wf_g.right,
+    height_gov_wf_g = 700 - margin_gov_wf_g.top - margin_gov_wf_g.bottom;
 
 // SVG chart container
-var svg_gov_g = d3.select("#gov-schemes")
+var svg_gov_wf_g = d3.select("#gov-schemes-wf")
     .append("svg")
-    .attr("width", width_gov_g + margin_gov_g.left + margin_gov_g.right)
-    .attr("height", height_gov_g + margin_gov_g.top + margin_gov_g.bottom)
+    .attr("width", width_gov_wf_g + margin_gov_wf_g.left + margin_gov_wf_g.right)
+    .attr("height", height_gov_wf_g + margin_gov_wf_g.top + margin_gov_wf_g.bottom)
     .append("g")
     .attr("transform",
-        "translate(" + margin_gov_g.left + "," + margin_gov_g.top + ")");
+        "translate(" + margin_gov_wf_g.left + "," + margin_gov_wf_g.top + ")");
 
 // Init x and y axes
-var x_gov_g = d3.scaleBand()
-    .range([0, width_gov_g])
+var x_gov_wf_g = d3.scaleBand()
+    .range([0, width_gov_wf_g])
     .padding([0.2]);
 
-var xAxis_gov_g = svg_gov_g.append("g")
-    .attr("transform", "translate(0," + height_gov_g + ")");
+var xAxis_gov_wf_g = svg_gov_wf_g.append("g")
+    .attr("transform", "translate(0," + height_gov_wf_g + ")");
 
-var y_gov_g = d3.scaleLinear()
+var y_gov_wf_g = d3.scaleLinear()
     .domain([0, 100])
-    .range([height_gov_g, 0]);
+    .range([height_gov_wf_g, 0]);
 
-var yAxis_gov_g = svg_gov_g.append("g");
+var yAxis_gov_wf_g = svg_gov_wf_g.append("g");
 
 // Update and draw chart data depending on filters
-function updateGovChart() {
+function updateGovWfChart() {
 
     var dataFile = "";
 
     // Button input defines data displayed
     if(document.getElementById("job-retention").checked){
-      dataFile = "assets/data/queries/gov-schemes/covid-job-retention-scheme.csv"
+      dataFile = "assets/data/queries/gov-schemes/covid-job-retention-scheme-wf.csv"
     }
     else if(document.getElementById("business-rates-holiday").checked){
-      dataFile = "assets/data/queries/gov-schemes/business-rates-holiday.csv"
+      dataFile = "assets/data/queries/gov-schemes/business-rates-holiday-wf.csv"
     }
     else if(document.getElementById("deferring-vat").checked){
-      dataFile = "assets/data/queries/gov-schemes/deferring-vat-payments.csv"
+      dataFile = "assets/data/queries/gov-schemes/deferring-vat-payments-wf.csv"
     }
     else if(document.getElementById("hmrc-time-to-pay").checked){
-      dataFile = "assets/data/queries/gov-schemes/hmrc-time-to-pay.csv"
+      dataFile = "assets/data/queries/gov-schemes/hmrc-time-to-pay-wf.csv"
     }
     else if(document.getElementById("small-business").checked){
-      dataFile = "assets/data/queries/gov-schemes/small-business-grant.csv"
+      dataFile = "assets/data/queries/gov-schemes/small-business-grant-wf.csv"
     }
     else if(document.getElementById("accredited-finance").checked){
-      dataFile = "assets/data/queries/gov-schemes/accredited-finance-agreements.csv"
+      dataFile = "assets/data/queries/gov-schemes/accredited-finance-agreements-wf.csv"
     }
     else if(document.getElementById("none").checked){
-      dataFile = "assets/data/queries/gov-schemes/none.csv"
+      dataFile = "assets/data/queries/gov-schemes/none-wf.csv"
     }
 
     // Parse CSV data
@@ -62,26 +62,26 @@ function updateGovChart() {
 
         // List of X axis groups + set X domain
         var groups = d3.map(data, function (d) {
-            return (d.industry)
+            return (d.workforce)
         }).keys();
-        x_gov_g.domain(groups);
+        x_gov_wf_g.domain(groups);
 
         // Add X and Y axis
-        xAxis_gov_g.call(d3.axisBottom(x_gov_g).tickSize(0))
+        xAxis_gov_wf_g.call(d3.axisBottom(x_gov_wf_g).tickSize(0))
             .selectAll("text")
             .style('text-anchor', 'start')
             .attr('transform', 'rotate(45 -10 10)');
 
-        yAxis_gov_g.transition().duration(1000).call(d3.axisLeft(y_gov_g));
+        yAxis_gov_wf_g.attr("transform", "translate(" + width_gov_wf_g + ",0)").transition().duration(1000).call(d3.axisRight(y_gov_wf_g));
 
         // Subgroup positioning scale
         var xSubgroup = d3.scaleBand()
             .domain(subgroups)
-            .range([0, x_gov_g.bandwidth()])
+            .range([0, x_gov_wf_g.bandwidth()])
             .padding([0.05]);
 
         // Show bar groups
-        var barGroups = svg_gov_g
+        var barGroups = svg_gov_wf_g
             .selectAll("g.layer")
             .data(data);
 
@@ -90,7 +90,7 @@ function updateGovChart() {
             .append("g")
             .classed('layer', true)
             .attr("transform", function (d) {
-                return "translate(" + x_gov_g(d.industry) + ",0)";
+                return "translate(" + x_gov_wf_g(d.workforce) + ",0)";
             });
 
         // Remove non-present groups
@@ -99,7 +99,7 @@ function updateGovChart() {
             .remove();
 
         // Show bars
-        var bars = svg_gov_g
+        var bars = svg_gov_wf_g
             .selectAll("g.layer")
             .selectAll("rect")
             .data(function (d) {
@@ -124,20 +124,20 @@ function updateGovChart() {
             .transition()
             .duration(1000)
             .attr("y", function (d) {
-                return y_gov_g(d.value);
+                return y_gov_wf_g(d.value);
             })
             .attr("height", function (d) {
-                return height_gov_g - y_gov_g(d.value);
+                return height_gov_wf_g - y_gov_wf_g(d.value);
             });
 
         bars
             .transition()
             .duration(1000)
             .attr("y", function (d) {
-                return y_gov_g(d.value);
+                return y_gov_wf_g(d.value);
             })
             .attr("height", function (d) {
-                return height_gov_g - y_gov_g(d.value);
+                return height_gov_wf_g - y_gov_wf_g(d.value);
             });
 
         // Remove non-present bars
@@ -148,23 +148,5 @@ function updateGovChart() {
     })
 }
 
-// Change the tooltip when user hovers / moves / leaves a cell
-  var mouseover_gov_g = function(d) {
-    var subgroupName = d3.select(this).datum().key;
-    var capSubgroup = subgroupName.charAt(0).toUpperCase() + subgroupName.slice(1)
-    tooltip
-        .html(capSubgroup + "<br>" + "Value: " + d.value + "%")
-        .style("opacity", 1)
-  }
-  var mousemove_gov_g = function(d) {
-    tooltip
-      .style("left", (d3.event.pageX+30) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-      .style("top", (d3.event.pageY+30) + "px")
-  }
-  var mouseleave_gov_g = function(d) {
-    tooltip
-      .style("opacity", 0)
-  }
-
 // Init chart on page load
-updateGovChart();
+updateGovWfChart();
